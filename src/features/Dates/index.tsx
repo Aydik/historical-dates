fd4875e506088d6f1ed166a14a7f47b2ef69855b
Aydik <Nekkisuper@gmail.com>
2025-09-19 16:10:33 +0300

    fix(Dates): Анимированные даты

--- src/features/Dates/index.tsx
+++ src/features/Dates/index.tsx
@@ -1,15 +1,36 @@
 import { FC } from 'react';
 import styles from './index.module.scss';
-import { useHistoricalEventsStore } from '@widgets/HistoricalEvents/stores/historicalEvents.store';
-import classNames from 'classnames';
+import { AnimatedYear } from '@features/Dates/ui/AnimatedYear';
+import { useBreakpoint } from '@shared/context/Breakpoints';
 
 export const Dates: FC = () => {
-  const { dateFrom, dateTo } = useHistoricalEventsStore();
+  const breakpoint = useBreakpoint();
+
+  let fontSize: number;
+  switch (breakpoint) {
+    case 'xxl':
+      fontSize = 200;
+      break;
+    case 'xl':
+    case 'lg':
+      fontSize = 160;
+      break;
+    case 'md':
+      fontSize = 120;
+      break;
+    case 'sm':
+      fontSize = 80;
+      break;
+    case 'xs':
+    default:
+      fontSize = 56;
+      break;
+  }
   return (
-    <div className={styles.dates}>
-      <p className={classNames(styles.date, styles.dateFrom)}>
-        {dateFrom} {dateTo}
-      </p>
-    </div>
+    <p className={styles.date} style={{ fontSize: fontSize }}>
+      <AnimatedYear type="prev" />
+      {'  '}
+      <AnimatedYear type="next" />
+    </p>
   );
 };
---
+++ src/features/Dates/ui/AnimatedYear/index.tsx
@@ -0,0 +1,52 @@
+import { FC, useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+import { useHistoricalEventsStore } from '@widgets/HistoricalEvents/stores/historicalEvents.store';
+
+interface Props {
+  type: 'prev' | 'next';
+  duration?: number;
+  ease?: string;
+}
+
+export const AnimatedYear: FC<Props> = ({ type, duration = 0.7, ease = 'power2.out' }) => {
+  const { dateFrom, dateTo } = useHistoricalEventsStore();
+  const year = type === 'prev' ? dateFrom : dateTo;
+
+  const elementRef = useRef<HTMLSpanElement>(null);
+  const prevYearRef = useRef<number>(year);
+
+  useEffect(() => {
+    if (!elementRef.current || prevYearRef.current === year) return;
+
+    const oldYear = prevYearRef.current;
+    const newYear = year;
+
+    const direction = oldYear < newYear ? 1 : -1;
+    const yearDifference = Math.abs(newYear - oldYear);
+
+    gsap.to(
+      {},
+      {
+        duration: duration,
+        ease: ease,
+        onUpdate: function () {
+          if (!elementRef.current) return;
+
+          const progress = this.progress();
+          const currentYear = Math.floor(oldYear + yearDifference * progress * direction);
+
+          elementRef.current.textContent = String(currentYear);
+        },
+        onComplete: () => {
+          if (elementRef.current) {
+            elementRef.current.textContent = String(newYear);
+          }
+        },
+      },
+    );
+
+    prevYearRef.current = year;
+  }, [year, duration, ease]);
+
+  return <span ref={elementRef}>{year}</span>;
+};
